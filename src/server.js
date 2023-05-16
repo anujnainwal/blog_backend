1233cbc36cd54212b829e874fbdc14a77f5c5ef3
asndevil <anujsinghnainwal@gmail.com>
2023-05-16 11:12:53 +0530

    update code

--- src/server.js
+++ src/server.js
@@ -3,9 +3,7 @@ const bodyParser = require("body-parser");
 const { config, corsOption } = require("../config/config.js");
 const cors = require("cors");
 const helmet = require("helmet");
-const morgan = require("morgan");
-const path = require("path");
-const fs = require("fs");
+const compression = require("compression");
 const { notFound404, errorHandler } = require("../middleware/errors.js");
 const database = require("../config/database.js");
 const app = express();
@@ -13,16 +11,19 @@ const app = express();
 console.log("Node Server-> ", config.NODE_ENVIROMENT);
 database();
 //create access logs file in node js
-let accessLogFile = fs.createWriteStream(
-  path.join(__dirname, "..", "/logs", "accessLog.log"),
-  { flags: "a" }
-);
 
-morgan.token("ist", (req, res, tz) => {
-  const now = new Date().toLocaleString("en-US", { timeZone: tz });
-  return now + " IST";
-});
 app.use(cors(corsOption));
+app.use(compression({ filter: shouldCompress, level: 9 }));
+
+function shouldCompress(req, res) {
+  if (req.headers["x-no-compression"]) {
+    // don't compress responses with this request header
+    return false;
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res);
+}
 app.use(helmet());
 app.use(bodyParser.json({ limit: "10mb" }));
 
@@ -35,18 +36,7 @@ app.use(
 );
 
 //wriet log file
-app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms [:ist[Asia/Kolkata]]",
-    { stream: accessLogFile }
-  )
-);
 
-app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms [:ist[Asia/Kolkata]]"
-  )
-);
 app.use((req, res, next) => {
   res.removeHeader("X-powered-by", false);
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -62,7 +52,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res, next) => {
-  return res.status(200).send("Hello world");
+  return res.status(200).json({ message: "OK" });
 });
 
 //routes defined here.
@@ -70,6 +60,7 @@ app.use("/api/v1/user", require("../router/user.routes.js"));
 app.use("/api/v1/post", require("../router/post.routes.js"));
 app.use("/api/v1/comment", require("../router/comment.routes.js"));
 app.use("/api/v1/category", require("../router/category.routes.js"));
+
 //not found error Handler
 app.use(notFound404);
 //errorHandler
